225f78eef8aae8580de18f39216f497b4bc4cf81
Courtney Phillips <courtney@epicodus.com>
2016-09-01 15:11:43 -0700

    Add basic command line interface.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,13 @@
+import java.io.Console
+
 public class App {
   public static void main(String[] args) {
-
-    System.out.println("I'm a ping-pong application!");
-
+    Console myConsole = System.console();
+    System.out.println("Enter a number:");
+    String stringUserNumber = myConsole.readLine();
+    int intUserNumber = Integer.parseInt(stringUserNumber);
+    PingPong pingPong = new PingPong();
+    List<Object> pingPongResult = pingPong.runPingPong(intUserNumber);
+    System.out.println(pingPongResult);
   }
 }
