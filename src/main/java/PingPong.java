96d0bb3eb52cb41e8b9cde03b81f831c3a27dc6f
Courtney Phillips <courtney@epicodus.com>
2016-09-01 15:03:30 -0700

    Add logic to replace numbers in the sequence divisible by both 3 and 5 with 'pingpong'.

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -6,7 +6,9 @@ public class PingPong {
   public List<Object> runPingPong(int countUpTo){
     List<Object> result = new ArrayList<Object>();
     for (int i = 1; i <= countUpTo; i++){
-      if (i % 3 == 0){
+      if (i % 3 == 0 && i % 5 == 0){
+        result.add("pingpong");
+      } else if (i % 3 == 0){
         result.add("ping");
       } else if (i % 5 == 0){
         result.add("pong");
