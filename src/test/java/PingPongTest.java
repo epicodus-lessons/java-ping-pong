d5777a58b990f9cd8d5af31ad8a14aacc2f6f8b5
Courtney Phillips <courtney@epicodus.com>
2016-09-01 15:00:25 -0700

    Write spec to assert that numbers divisible by both 3 and 5 are replaced with 'pingpong'.

--- src/test/java/PingPongTest.java
+++ src/test/java/PingPongTest.java
@@ -44,4 +44,26 @@ public class PingPongTest {
     assertEquals(expectedOutput, testPingPong.runPingPong(5));
   }
 
+  @Test
+  public void runPingPong_replaceMultiplesOfBoth3And5_ArrayList() {
+    PingPong testPingPong = new PingPong();
+    List<Object> expectedOutput = new ArrayList<Object>();
+    expectedOutput.add(1);
+    expectedOutput.add(2);
+    expectedOutput.add("ping");
+    expectedOutput.add(4);
+    expectedOutput.add("pong");
+    expectedOutput.add("ping");
+    expectedOutput.add(7);
+    expectedOutput.add(8);
+    expectedOutput.add("ping");
+    expectedOutput.add("pong");
+    expectedOutput.add(11);
+    expectedOutput.add("ping");
+    expectedOutput.add(13);
+    expectedOutput.add(14);
+    expectedOutput.add("pingpong");
+    assertEquals(expectedOutput, testPingPong.runPingPong(15));
+  }
+
 }
